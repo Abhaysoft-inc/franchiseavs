a4482affd4b9f4eb5092b5e1f429294759c80008
Abhay Vishwakarma <av385735@gmail.com>
2024-04-08 00:20:04 +0530

    a

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <>
+
+      <nav className="navbar bg-white shadow-lg">
+        <ul className='py-[17px]'>
+            <li className='Brand ml-[82px]'><img src="src\assets\images\cropped-Franchise-AVS-180-x-60-px-9.png" alt="" /></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+      </nav>
+      
+      
+      
+      </>
+    )
+  }
+}
