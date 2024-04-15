926ebd83df5480f7cd929add14f24e03a5da70d3
Abhaysoft-inc <av385735@gmail.com>
2024-04-15 23:40:26 +0530

    a

--- src/screens/BlogScreen.jsx
+++ src/screens/BlogScreen.jsx
@@ -1,9 +1,44 @@
 import React, { Component } from 'react'
 
 export default class BlogScreen extends Component {
-  render() {
-    return (
-      <div>BlogScreen</div>
-    )
-  }
+    render() {
+        return (
+            <>
+
+                <div className='mt-10'>
+                    <p className="text-center text-2xl font-[600]">Our Latest Posts</p>
+                </div>
+
+                <div className="flex flex-wrap mx-10 mt-5">
+
+                    <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-4">
+                        <div className="bg-white rounded-lg overflow-hidden shadow-md">
+                            <img className="w-full h-32 object-cover object-center" src="/assets/images/Zudio.jpg" alt="zudio in rajouri garden, zudio rajouri garden, zudio franchise enquiry, zudio franchise cost, zudio franchise fees, zudio franchise eligibility, zudio franchise price, zudio franchise criteria, how to get zudio franchise, zudio franchies" />
+                            <div className="px-4 py-2">
+                                <h2 className="text-lg font-semibold text-gray-800"><a href="/zudio-franchise">Zudio Franchise Cost, Fee, Details and Review</a></h2>
+                                <p className="text-sm text-gray-600 mt-1">Zudio, a brand under the Tata Group, offers affordable and stylish fashion for the whole family. Launched in 2016, it has quickly become a go-to destination for budget-conscious shoppers in India. With a wide range of clothing and accessories, Zudio provides trendy options without breaking the bank.</p>
+                                
+                                
+                            </div>
+                        </div>
+                    </div>
+
+                    
+                    
+
+
+                    
+
+
+                    
+
+                </div>
+
+
+
+
+
+            </>
+        )
+    }
 }
